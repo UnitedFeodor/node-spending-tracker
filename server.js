6075ff46d17e86fd97d08c53af69399dbbe2a11e
feodor <fedor15zil@gmail.com>
2023-02-16 00:31:58 +0300

    rename stuff

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ app.listen(3000)
 app.set('view engine', 'ejs')
 app.use('/public/', express.static('./public'));
 
-const taskRouter = require('./routes/taskmanager')
-app.use('/taskmanager',taskRouter)
+const taskRouter = require('./routes/spending')
+app.use('/',taskRouter)
