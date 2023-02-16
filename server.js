6b2d1b4a41936738646a67b0d73ebf37150ecbec
feodor <fedor15zil@gmail.com>
2023-02-16 15:42:11 +0300

    parse int for amount from add

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express()
 app.listen(3000)
 app.set('view engine', 'ejs')
 app.use('/public/', express.static('./public'));
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 
